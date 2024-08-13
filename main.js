16d2d2c60b1e84c6b57f53a0c33a599800ee9e81
Victor Pereira <victor386pereira@gmail.com>
2024-08-13 18:57:42 -0300

    :tada: MOBILE and FINISH

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+function openMenu() {
+  let mobileMenu = document.querySelector(".navItem-mobile");
+  if (mobileMenu.classList.contains("open")) {
+    mobileMenu.classList.remove("open");
+    document.querySelector(".menuIcon").src = "./assets/menuIcon.png";
+  } else {
+    mobileMenu.classList.add("open");
+    document.querySelector(".menuIcon").src = "./assets/icons8-excluir-50.png";
+  }
+}
+
 var elementosDuvida = document.querySelectorAll(".questionForm-resposta");
 
 elementosDuvida.forEach(function (duvida) {
